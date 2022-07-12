a2d6a446201a205a0b1144dad6ecda9000248d1c
OEM Configuration (temporary user) <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>
2022-07-12 19:33:15 +0100

    chess layer and print board

--- chess-system/src/main/java/application/Program.java
+++ chess-system/src/main/java/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
-import boardGame.Board;
-import boardGame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
-        System.out.println(board);
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(ChessMatch.getPieces());
+        System.out.println();
     }
 }
