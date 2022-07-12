e2715ea84ad3c76824eead523b512be8994b11ad
OEM Configuration (temporary user) <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>
2022-07-12 18:53:27 +0100

    created boardGame/Position class

---
+++ chess-system/src/main/java/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+        System.out.println("Hello world!");
+    }
+}
